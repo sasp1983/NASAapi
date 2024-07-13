141c9acd7ef4645cb2318efe09ba913d2df7aa69
sasp1983 <96437889+sasp1983@users.noreply.github.com>
2024-07-13 02:54:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,106 +6,178 @@ let i = 0;
 
 let imgArray = []
 let pageNumber = 1;
-let pictureGrid = document.querySelector('.picture-grid');  
-let page = document.querySelector('.page');
+let pictureGrid = document.querySelector('.picture-grid');
+// let page = document.querySelector('.page');
 let images = document.querySelectorAll('.grid-img');
+let pageBtnContainer = document.querySelector('.pagenumber-container')
+let buttons = document.querySelectorAll('.pagebtn');
+let solDateInput = document.getElementById('sol-date');
+let solDate = 1000;
 
-async function fetchData() {
+async function fetchPageCount() {
     try {
-        const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${pageNumber}&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ`);
+        const responseNoPages = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solDate}&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ`);
+
+        if (!responseNoPages.ok) {
+            throw new Error("ADJAS")
+        }
+
+        const dataNoPages = await responseNoPages.json();
+        // console.log(dataNoPages);
+        console.log('array length:', dataNoPages.photos.length);
+        let pageCount = Math.ceil(dataNoPages.photos.length / 25);
+        console.log('pageCount:', pageCount);
+
+        arrowRightBtn.addEventListener('click', function () {
+            // fetchPageCount();
+            if (pageNumber < pageCount) {
+                pageNumber += 1;
+                fetchImages();
+                // page.innerText = `page: ${pageNumber}`
+                window.scroll({ top: 0, behavior: 'smooth' });
+            }
+            console.log(pageCount)
+        });
+
+        arrowLeftBtn.addEventListener('click', function () {
+            if (pageNumber >= 2) {
+                pageNumber--;
+                fetchImages();
+                // page.innerText = `page: ${pageNumber}`
+                window.scroll({ top: 0, behavior: 'smooth' });
+            }
+        });
+
+        function hideButtons() {
+            if (pageBtnContainer.childElementCount > pageCount) {
+                buttons.forEach(p => {
+                    if (p.innerText > pageCount) {
+                        p.classList.add('hidden');
+                    } else {
+                        p.classList.remove('hidden');
+                        // p.style.visibility = 'visible'
+                    }
+                });
+            }
+        }
+        hideButtons();
+
+        // function createButtons() {
+        //     for (let p = 0; p < pageCount; p++) {
+        //         let bu = document.createElement('button');
+        //         bu.classList.add('pagebtn');
+        //         pageBtnContainer.appendChild(bu);
+        //         bu.innerText = `${p + 1}`;
+        //         bu.id = p + 1;
+        //         console.log(p);
+        //     }
+        // }
+        // createButtons();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function fetchImages() {
+    try {
+        const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solDate}&page=${pageNumber}&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ`);
+
+
+        // const responseNoPages = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solDate}&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ`);
 
         if (!response.ok) {
             throw new Error("ADJAS")
         }
         const data = await response.json();
-        console.log(response.url);
+        // const dataNoPages = await responseNoPages.json();
+        // console.log(dataNoPages);
+        // console.log(dataNoPages.photos.length);
+
+        // let pageCount = Math.ceil(dataNoPages.photos.length / 25);
+        // console.log(pageCount);
+
+        // console.log(response.url);
         let imgArray = data.photos;
         // console.log(response)
         // console.log(data)
-        
+
         for (i = 0; i < imgArray.length; i++) {
             let images = document.querySelectorAll('.grid-img');
             images[i].src = imgArray[i].img_src;
             // console.log(i) ;     
             let imgLink = document.querySelectorAll('a');
             imgLink[i].src = imgArray[i].img_src;
-            imgLink[i].href = imgArray[i].img_src;           
+            imgLink[i].href = imgArray[i].img_src;
             images[i].onload = images[i].style.opacity = '100%';
 
-            buttons.forEach(p => {
-                if (p.id == pageNumber) {
-                 p.classList.add('current-page')
-                } else {
-                 p.classList.remove('current-page')
-                }
-             })
-            
-        }      
-    }  catch (error) {
+            //highlight page buttons
+            highlightButtons = () => {
+                buttons.forEach(p => {
+                    if (p.id == pageNumber) {
+                        p.classList.add('current-page')
+                    } else {
+                        p.classList.remove('current-page')
+                    }
+                })
+            }
+            highlightButtons();
+        }
+
+    } catch (error) {
         console.error(error)
-    } 
-}
-
-
-fetchData();
-    page.innerText = `page: ${pageNumber}`
-    
-    
-arrowRightBtn.addEventListener('click', function () {    
-    if (pageNumber < 35) {
-    pageNumber++;
-    fetchData();
-    page.innerText = `page: ${pageNumber}`
-    window.scroll({top:0,behavior:'smooth'});
     }
-});
-
-arrowLeftBtn.addEventListener('click', function() {
-    if (pageNumber >= 2) {
-    pageNumber--;
-    fetchData();
-    page.innerText = `page: ${pageNumber}`
-    window.scroll({top:0,behavior:'smooth'});
+    // fetchPageCount();
 }
-});
+
+fetchImages();
+fetchPageCount();
+
+// page.innerText = `page: ${pageNumber}`
 
 
+// arrowRightBtn.addEventListener('click', function () {
+//     fetchPageCount();
+//     if (pageNumber < pageCount) {
+//         pageNumber++;
+//         fetchImages();
+//         // page.innerText = `page: ${pageNumber}`
+//         window.scroll({ top: 0, behavior: 'smooth' });
+//     }
+// });
 
-// pagebtn.addEventListener('click', function() {
-//     pageNumber = 1;
-//     fetchData();
-//      page.innerText = `page: ${pageNumber}`
-//     console.log(pageNumber)
-// }) 
+// arrowLeftBtn.addEventListener('click', function () {
+//     if (pageNumber >= 2) {
+//         pageNumber--;
+//         fetchImages();
+//         // page.innerText = `page: ${pageNumber}`
+//         window.scroll({ top: 0, behavior: 'smooth' });
+//     }
+// });
 
-let buttons = document.querySelectorAll('.pagebtn');
 
+//make buttons go to page number
 buttons.forEach(pagebtn => {
-    pagebtn.addEventListener('click', function(e) {
-    console.log(e.target)
-    pageNumber = pagebtn.id;
-    page.innerText = `page: ${pageNumber}`  
-    fetchData();   
-    }) 
+    pagebtn.addEventListener('click', function (e) {
+        pageNumber = pagebtn.id;
+
+        // page.innerText = `page: ${pageNumber}`
+        fetchImages();
+    })
+
+
+})
+
+solDateInput.addEventListener('change', function (e) {
+    e.preventDefault;
+    console.log('sol date:', solDateInput.value);
+    let solValue = solDateInput.value;
+    solDate = solValue;
+    fetchImages();
+    fetchPageCount();
+    pageNumber = 1;
+
 })
 
 
 
 
-// async function fetchData() {
-//     try {
-//         const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=TDRKWxcci6VXt53Z5NocxnQTtTg6N2fsiSZOR8dQ`);
-
-//         if (!response.ok) {
-//             throw new Error("ADJAS")
-//         }
-//         const data = await response.json();
-//         console.log(data)
-        
-      
-//         }  catch (error) {
-//         console.error(error)
-//     } 
-// }
-
-// fetchData()
